6c22f60d2e456a09dbb2cdcff6ada87011cd839d
Cari Lewis <cdl5261@utulsa.edu>
2020-10-01 17:14:44 -0500

    Added functional code that creates a file named by each snake and adds headers into each file

--- assign3/Lewis_assign3.sh
+++ assign3/Lewis_assign3.sh
@@ -2,26 +2,25 @@
 
 #script that creates a file for each BTS in the BTS_data.txt file named with the BTS ID
 
-#create a file that lists all the unique snake IDs
-cut -f 1 BTS_data.txt | sort -k1 | uniq > ID.txt
+#create a file that lists all the unique snake IDs without the ID header
+cut -f 1 BTS_data.txt | grep 13BTS[0-9] | uniq | sort -k1 > ID.txt
 
-#create an array that names a file for each snake in ID.txt
+#create an array that names a file for each unique snake and add headers
 while read line
 	do touch file_$line.txt
+	head -1 BTS_data.txt >> file_$line.txt
 done < ID.txt
 
 #create an array that reads the lines in the ID.txt file
 snake=0
 
 while read file
-	do array[$snake]=$file
+	do grep array[$snake]=$file
 	let snake=snake+1
-done < ID.txt
+done < BTS_data.txt
 
 #use the array to copy all the snake data into their respective files
 for ID in ${array[*]}
-	do grep 13BTS$ID >> file_13BTS$ID.txt
+	do grep $ID >> file_$ID.txt
 done
 
-#list and count the number of files created
-ls | count
